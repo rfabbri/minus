1eb67d83f9124fae78cb2c7292590cd5b8d28f3a
Ricardo Fabbri <rfabbri@gmail.com>
2019-02-25 13:22:34 -0300

    Eigen library

---
+++ Eigen/src/Core/util/Meta.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ * 
+ *        Filename:  Meta.c
+ * 
+ *     Description:  JK
+ * 
+ *         Version:  1.0
+ *         Created:  02/09/2019 05:09:58 EST
+ *        Revision:  none
+ *        Compiler:  gcc
+ * 
+ *          Author:  Ricardo Fabbri (rfabbri), rfabbri.github.io
+ *         Company:  Brown University
+ * 
+ * =====================================================================================
+ */
+
+
