4e1e87f6df62ccf941da0d3f7858edbc6a4d4b6f
Ricardo Fabbri <rfabbri@gmail.com>
2019-03-08 15:19:45 -0300

    2.2s chicago14 minimal memory

--- minus.cxx
+++ minus.cxx
@@ -662,31 +662,31 @@ ptrack_subset(const TrackerSettings *s, const complex s_sols[NNN*NSOLS], const c
   const double t_step = s->init_dt_;  // initial step
   complex x0t0[NNNPLUS1];  // t = real running in [0,1]
   complex *const x0 = x0t0; double *const t0 = (double *) (x0t0 + NNN);
-  //  complex* x1 =  x1t1;
-  //  complex* t1 = x1t1+NNN;
   complex dxdt[NNNPLUS1], *const dx = dxdt; double *const dt = (double *)(dxdt + NNN);
   complex Hxt[NNNPLUS1 * NNN], *const HxH=Hxt;  // HxH is reusing Hxt
   const complex * const RHS = Hxt + NNN2;  // Hx or Ht, same storage
   complex * const LHS = Hxt;
   complex xt[NNNPLUS1];
-  complex dx1[NNN], dx2[NNN], dx3[NNN];
-  complex *const dx4 = dx; // reuse dx for dx4
+  complex dxi[NNN];
+  complex *const dx4 = dx;   // reuse dx for dx4
   complex *const x1t1 = xt;  // reusing xt's space to represent x1t1
   using namespace Eigen;
-  Map<Matrix<complex, NNN, 1> > dx1_eigen(dx1);
-  Map<Matrix<complex, NNN, 1> > dx2_eigen(dx2);
-  Map<Matrix<complex, NNN, 1> > dx3_eigen(dx3);
+  Map<Matrix<complex, NNN, 1> > x0_eigen(x0);
+  Map<Matrix<complex, NNNPLUS1, 1> > x0t0_eigen(x0t0);
+  Map<Matrix<complex, NNN, 1> > dxi_eigen(dxi);
   Map<Matrix<complex, NNN, 1> > dx4_eigen(dx4);
   Map<Matrix<complex, NNN, 1> > &dx_eigen = dx4_eigen;
   Map<Matrix<complex, NNN, NNN> > AA((complex *)Hxt,NNN,NNN);  // accessors for the data
   Map<const Matrix<complex, NNN, 1> > bb(RHS);
+  Map<Matrix<complex, NNN, 1> > xt_NNN_eigen(xt);
+  Map<Matrix<complex, NNNPLUS1, 1> > xt_NNNPLUS1_eigen(xt);
+  Map<Matrix<complex, NNNPLUS1, 1> > &x1t1_eigen = xt_NNNPLUS1_eigen;
+  Map<Matrix<complex, NNN, 1> > &x1_eigen = xt_NNN_eigen;
+  Map<Matrix<complex, NNNPLUS1, 1> > dxdt_eigen(dxdt);
 
   Solution* t_s = raw_solutions + sol_min;  // current target solution
   const complex* s_s = s_sols + sol_min*NNN;    // current start solution
-  for (unsigned sol_n = sol_min; sol_n < sol_max; ++sol_n) { // outer loop
-    #ifdef M_VERBOSE
-    std::cerr << "Trying solution #" << sol_n << std::endl;
-    #endif
+  for (unsigned sol_n = sol_min; sol_n < sol_max; ++sol_n) { // solution loop
     t_s->status = PROCESSING;
     bool end_zone = false;
     array_copy(s_s, x0);
@@ -694,10 +694,6 @@ ptrack_subset(const TrackerSettings *s, const complex s_sols[NNN*NSOLS], const c
     *dt = t_step;
     unsigned predictor_successes = 0;
 
-    // print start solution
-
-    // PASS array_print("s_s",s_s);
-    
     // track H(x,t) for t in [0,1]
     while (t_s->status == PROCESSING && 1 - *t0 > the_smallest_number) {
       if (!end_zone && 1 - *t0 <= s->end_zone_factor_ + the_smallest_number)
@@ -705,182 +701,50 @@ ptrack_subset(const TrackerSettings *s, const complex s_sols[NNN*NSOLS], const c
       if (end_zone) {
           if (*dt > 1 - *t0) *dt = 1 - *t0;
       } else if (*dt > 1 - s->end_zone_factor_ - *t0) *dt = 1 - s->end_zone_factor_ - *t0;
-      // PREDICTOR in: x0t0,dt
-      //           out: dx
-      // Runge Kutta
+      // PREDICTOR in: x0t0,dt out: dx
       /*  top-level code for Runge-Kutta-4
           dx1 := solveHxTimesDXequalsMinusHt(x0,t0);
           dx2 := solveHxTimesDXequalsMinusHt(x0+(1/2)*dx1*dt,t0+(1/2)*dt);
           dx3 := solveHxTimesDXequalsMinusHt(x0+(1/2)*dx2*dt,t0+(1/2)*dt);
           dx4 := solveHxTimesDXequalsMinusHt(x0+dx3*dt,t0+dt);
-          (1/6)*dt*(dx1+2*dx2+2*dx3+dx4)
-      */
-      #ifdef M_VERBOSE
-      std::cerr << "\tEntered while loop" << std::endl;
-      std::cerr << "t0 real" << *t0 << std::endl;
-      #endif
-      
-      // bool Axb_success = true;
+          (1/6)*dt*(dx1+2*dx2+2*dx3+dx4) */
       array_copy_NNNplus1(x0t0, xt);
 
-      #ifdef M_VERBOSE
-      array_print_NNNplus1("x0t0", x0t0);
-      array_print_NNNplus1("xt", xt);
-      array_print_n("xt(params)", xt+NNNPLUS1,2*NPARAMS);
-      #endif
-      
       // dx1
       evaluate_Hxt(xt, params, Hxt); // Outputs Hxt
-      #ifdef M_VERBOSE
-      array_print_H("Hxt",Hxt);
-
-      if (std::abs(Hxt[0] - complex(-.0142658,+.0314315)) <= dbgtol &&
-          std::abs(Hxt[NNN*NNN-1] - complex(0,0)) <= dbgtol)
-        std::cerr << "Hx from Hxt PASS\n";
-      else
-        std::cerr << "Hx from Hxt FAIL\n";
-      
-      if (std::abs(Hxt[NNN*NNN] - complex(.0901308,.0653859)) <= dbgtol &&
-          std::abs(Hxt[NNN*NNN+NNN-1] - complex(-.0644011,-.470543)) <= dbgtol)
-        std::cerr << "Ht from Hxt PASS \n";
-      else {
-        std::cerr << "Ht from Hxt FAIL\n";
-        std::cerr << "\t" << Hxt[NNN*NNN] - complex(.0901308,.0653859)
-          << Hxt[NNN*NNN+NNN-1] - complex(-.0644011,-.470543);
-      }
-      #endif
-      
-      #ifdef M_VERBOSE
-      array_print_n("LHS",LHS, NNN*NNN);
-      array_print("minus RHS",RHS);
-      #endif
-      // was: solve_via_lapack_without_transposition(n, LHS, 1, RHS, dx1);
       PartialPivLU<Matrix<complex, NNN, NNN> > lu(AA);
-      dx1_eigen = lu.solve(bb);
-      // Axb_success &= linear(LHS,RHS,dx1);
-      #ifdef M_VERBOSE
-      array_print("dx1",dx1);
-      // TODO: once this is working, use eigen2 for LU partial pivots, faster.
-      // this is QR full collumn pivoting, which is as fast as lapack LU
-      
-      if (std::abs(dx1[0] - complex(120.129,-61.1552)) <= dbgtol &&
-          std::abs(dx1[NNN-1] - complex(25.3738,3.0885)) <= dbgtol)
-        std::cerr << "dx1 PASS\n";
-      else
-        std::cerr << "dx1 FAIL\n";
-
-      #endif
+      dx4_eigen = lu.solve(bb);
       
       // dx2
       complex one_half_dt = *dt*0.5;
-      array_multiply_scalar_to_self(dx1, one_half_dt);
-      array_add_to_self(xt, dx1); // x0+.5dx1*dt
+      dx4_eigen *= one_half_dt;
+      xt_NNN_eigen += dx4_eigen;
+      dx4_eigen *= 2;
       xt[NNN] += one_half_dt;  // t0+.5dt
-      //
       evaluate_Hxt(xt, params, Hxt);
-      
-      lu.compute(AA); dx2_eigen = lu.solve(bb);
-      // Axb_success &= linear(LHS,RHS,dx2);
-      #ifdef M_VERBOSE
-      std::cerr << "second eval ---------" << std::endl;
-      array_print_NNNplus1("xt", xt);
-      array_print_H("Hxt",Hxt);
-      array_print("dx2", dx2);
-      
-      if (std::abs(dx2[0] - complex(414.419,-394.218)) <= dbgtol &&
-          std::abs(dx2[NNN-1] - complex(-246.31,-120.222)) <= dbgtol)
-        std::cerr << "dx2 PASS\n";
-      else
-        std::cerr << "dx2 FAIL\n";
-      #endif
+      dxi_eigen = lu.compute(AA).solve(bb);
 
       // dx3
-      array_multiply_scalar_to_self(dx2, one_half_dt);
-      array_copy(x0t0, xt);  // spare t
-      array_add_to_self(xt, dx2); // x0+.5dx2*dt
-      // xt[n] += one_half*(*dt); // t0+.5dt (SAME)
-      //
+      dxi_eigen *= one_half_dt;
+      dx4_eigen += dxi_eigen*4;
+      xt_NNN_eigen = x0_eigen + dxi_eigen;
       evaluate_Hxt(xt, params, Hxt);
-      #ifdef M_VERBOSE
-      array_print_H_full(Hxt);
-      #endif
-      // Axb_success &= linear(LHS,RHS,dx3);
-      lu.compute(AA); dx3_eigen = lu.solve(bb);
-      #ifdef M_VERBOSE
-      std::cerr << "third eval ---------" << std::endl;
-      array_print_NNNplus1("xt", xt);
-      array_print_H("Hxt",Hxt);
-      array_print("dx3", dx3);
-      if (std::abs(dx3[0] - complex(-11197.9,+14804.2)) <= 10*dbgtol &&
-          std::abs(dx3[NNN-1] - complex(-17653.8,-4747.93)) <= dbgtol)
-        std::cerr << "dx3 PASS\n";
-      else {
-        std::cerr << "dx3 FAIL\n";
-        std::cerr << "\t\t" << dx3[0] - complex(-11197.9,+14804.2)
-          << dx3[NNN-1] - complex(-17653.8,-4747.93) << std::endl;
-      }
-      #endif
+      dxi_eigen = lu.compute(AA).solve(bb);
 
       // dx4
-      array_multiply_scalar_to_self(dx3, *dt);
+      dxi_eigen *= *dt;
       array_copy_NNNplus1(x0t0, xt);
-      array_add_to_self(xt, dx3); // x0+dx3*dt
-      xt[NNN] += *dt;               // t0+dt
-      //
+      xt_NNN_eigen = x0_eigen + dxi_eigen;
+      dx4_eigen += dxi_eigen*2;
+      xt[NNN] = *t0 + *dt;               // t0+dt
       evaluate_Hxt(xt, params, Hxt);
-      #ifdef M_VERBOSE
-      std::cerr << "fourth eval ---------" << std::endl;
-      array_print_NNNplus1("xt", xt);
-        std::cerr << "\txt full: {" << std::setprecision(20);
-      for (unsigned id=0; id < NNN; ++id) {
-        std::cerr << "{" << xt[id] << "} ,";
-      }
-      std::cerr << "{" << xt[NNN]  << "}};" << std::endl;
-      
-      if (std::abs(Hxt[0] - complex(72802.2,-81551.9)) <= 10*dbgtol &&
-          std::abs(Hxt[NNN*NNN-1] - complex(0,0)) <= dbgtol)
-        std::cerr << "Hx from Hxt PASS\n";
-      else
-        std::cerr << "Hx from Hxt FAIL\n";
-      
-      if (std::abs(Hxt[NNN*NNN] - complex(-248323000,+163093000)) <= 1e3 &&
-          std::abs(Hxt[NNN*NNN+NNN-1] - complex(-.221521,-.47423)) <= dbgtol)
-        std::cerr << "Ht from Hxt PASS \n";
-      else
-        std::cerr << "Ht from Hxt FAIL\n";
-      array_print_H("Hxt",Hxt);
-      array_print_H_full(Hxt);
-      #endif
-      // Axb_success &= linear(LHS,RHS,dx4);
-      lu.compute(AA); dx4_eigen = lu.solve(bb);
-      #ifdef M_VERBOSE
-      array_print_NNNplus1("xt", xt);
-      array_print("dx4", dx4);
-      
-      if (std::abs(dx4[0] - complex(885846,811668)) <= 1000*dbgtol &&
-          std::abs(dx4[NNN-1] - complex(823211,-2145640)) <= 1000*dbgtol)
-        std::cerr << "dx4 PASS\n";
-      else {
-        std::cerr << "dx4 FAIL\n";
-        std::cerr << "\t\t" << dx4[0] - complex(885846,811668)
-          << dx4[NNN-1] -  complex(823211,-2145640) << std::endl;
-      }
-      #endif
+      dx4_eigen = (dx4_eigen + lu.compute(AA).solve(bb)* *dt)*(1./6.);
 
-      // "dx1" = .5*dx1*dt, "dx2" = .5*dx2*dt, "dx3" = dx3*dt
-      // TODO: make this into a single function loop directly
-      array_multiply_scalar_to_self(dx4, *dt);
-      array_multiply_scalar_to_self(dx1, 2);
-      array_multiply_scalar_to_self(dx2, 4);
-      array_multiply_scalar_to_self(dx3, 2);
-      array_add_to_self(dx4, dx1);
-      array_add_to_self(dx4, dx2);
-      array_add_to_self(dx4, dx3);
-      array_multiply_scalar_to_self(dx4, 1./6.);
-
+      // "dx1" = .5*dx1*dt, "dx2" = .5*dx2*dt, "dx3" = dx3*dt. Eigen vectorizes this:
+      // dx4_eigen = (dx4_eigen* *dt + dx1_eigen*2 + dx2_eigen*4 + dx3_eigen*2)*(1./6.);
+      
       // make prediction
-      array_copy_NNNplus1(x0t0, x1t1);
-      array_add_to_self_NNNplus1(x1t1, dxdt);
+      x1t1_eigen = x0t0_eigen + dxdt_eigen;
       
       // CORRECTOR
       unsigned n_corr_steps = 0;
@@ -888,101 +752,33 @@ ptrack_subset(const TrackerSettings *s, const complex s_sols[NNN*NSOLS], const c
       do {
         ++n_corr_steps;
         evaluate_HxH(x1t1, params, HxH);
-        #ifdef M_VERBOSE
-        std::cerr << "\tCorrection step " << n_corr_steps << std::endl;
-        array_print_NNNplus1("x1t1",x1t1);
-        
-        if (n_corr_steps == 1) {
-          if (std::abs(HxH[0] - complex(26113200,11590100)) <= dbgtol &&
-              std::abs(HxH[NNN*NNN-1] - complex(0,0)) <= dbgtol)
-            std::cerr << "Hx from HxH PASS\n";
-          else
-            std::cerr << "Hx from HxH FAIL\n";
-          
-          if (std::abs(HxH[NNN*NNN] - complex(+1.741e11,-15110700000)) <= dbgtol &&
-              std::abs(HxH[NNN*NNN+NNN-1] - complex(-3.71869e-13,+4.6629e-15)) <= dbgtol)
-            std::cerr << "H from HxH PASS \n";
-          else
-            std::cerr << "H from HxH FAIL \n";
-        }
-        array_print_H("HxH",HxH);
-        #endif
-        
-        // Axb_success &= linear(LHS,RHS,dx);
-        lu.compute(AA); dx_eigen = lu.solve(bb);
-        #ifdef M_VERBOSE
-        array_print_n("LHS corr",LHS,NNN2);
-        array_print("RHS corr",RHS);
-        array_print("dx",dx);
-        if (n_corr_steps == 1) {
-          if (std::abs(dx[0] - complex(-2206.68,-2378.66)) <= dbgtol &&
-              std::abs(dx[NNN-1] - complex(-1731.72,+6593.05)) <= dbgtol)
-            std::cerr << "dx from first correction PASS \n";
-          else
-            std::cerr << "dx from first correction FAIL \n";
-        }
-        #endif
-        array_add_to_self(x1t1, dx);
-        is_successful = array_norm2(dx) < s->epsilon2_ * array_norm2(x1t1);
-        #ifdef M_VERBOSE
-        // printf("c: |dx|^2 = %lf\n",
-        // norm2_complex_array<ComplexField>(n,dx));
-        #endif
+        dx_eigen = lu.compute(AA).solve(bb);
+        x1_eigen += dx_eigen;
+        is_successful = dx_eigen.squaredNorm() < s->epsilon2_ * x1_eigen.squaredNorm();
       } while (!is_successful && n_corr_steps < s->max_corr_steps_);
       
       if (!is_successful) { // predictor failure
         predictor_successes = 0;
         *dt *= s->dt_decrease_factor_;
         if (*dt < s->min_dt_) t_s->status = MIN_STEP_FAILED; // slight difference to SLP-imp.hpp:612
-        #ifdef M_VERBOSE
-        std::cerr << "\tPred failure" << std::endl;
-        std::cerr << "decreasing dt: " << *dt;
-        #endif
       } else { // predictor success
         ++predictor_successes;
         array_copy_NNNplus1(x1t1, x0t0);
         if (predictor_successes >= s->num_successes_before_increase_) {
           predictor_successes = 0;
           *dt *= s->dt_increase_factor_;
-          #ifdef M_VERBOSE
-          std::cerr << "\tPred success" << std::endl;
-          std::cerr << "increasing dt: " << *dt;
-          #endif
         }
       }
-      if (array_norm2(x0) > s->infinity_threshold2_)
+      if (x0_eigen.squaredNorm() > s->infinity_threshold2_)
         t_s->status = INFINITY_FAILED;
-      // if (!Axb_success) t_s->status = SINGULAR;
-      #ifdef M_VERBOSE
-      // std::cerr << "\tAxb_success" << Axb_success << std::endl;
-      #endif
-    } // while 
+    } // while (t loop)
     // record the solution
     array_copy(x0, t_s->x);
     t_s->t = *t0;
     if (t_s->status == PROCESSING) t_s->status = REGULAR;
-    // evaluate_HxH(x0t0, HxH);
-    // cond_number_via_svd(HxH /*Hx*/, t_s->cond);
     ++t_s;
     s_s += NNN;
   } // outer solution loop
 
   return 0;  // in the past, n_sols was returned, which now is NSOLS
 }
-  
-#if 0
-  /*
-    printf(
-        "epsilon2 = %e, t_step = %lf, dt_min_dbl = %lf, dt_increase_factor_dbl "
-        "= %lf, dt_decrease_factor_dbl = %lf\n",
-        epsilon2,
-        t_step,
-        dt_min_dbl,
-        dt_increase_factor_dbl,
-        dt_decrease_factor_dbl);
-        */
-    return 0;
-}
-//int
-//linear_bundle
-#endif
