8e31e2b0472d7142485c63b4fef8578b9db7aa45
Ricardo Fabbri <rfabbri@gmail.com>
2019-03-16 23:25:02 -0300

    fully teplated: WORKS, no performance penalty

---
+++ Templates/minus-chicago17+double.cxx
@@ -0,0 +1,7 @@
+// Helps instantiate and reuse code,
+// when including the .hxx directly causes inefficiencies and slowdown
+// 
+// \author Ricardo Fabbri based on original code by Anton Leykin 
+// \date Created: Fri Feb  8 17:42:49 EST 2019
+#include <minus.hxx>
+template class minus_core<312, 14, 56, chicago14a, double>;
