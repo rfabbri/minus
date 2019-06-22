e53848f1b2548b28a2d646ec9a4d235140a6ddeb
Ricardo Fabbri <rfabbri@gmail.com>
2019-06-22 14:54:24 -0300

    vnl fixed templates for tests

---
+++ algo/tests/Templates/vnl_vector_fixed+complex_double.allsizes_minus-.cxx
@@ -0,0 +1,6 @@
+#include <vnl/vnl_complex.h>
+#include <vnl/vnl_vector_fixed.hxx>
+VNL_VECTOR_FIXED_INSTANTIATE(std::complex<double>, 14);
+VNL_VECTOR_FIXED_INSTANTIATE(std::complex<double>, 196);
+VNL_VECTOR_FIXED_INSTANTIATE(std::complex<float>, 14);
+VNL_VECTOR_FIXED_INSTANTIATE(std::complex<float>, 196);
--- algo/tests/test_driver.cxx
+++ algo/tests/test_driver.cxx
@@ -1,12 +1,12 @@
 #include <testlib/testlib_register.h>
 
-DECLARE( test_expminus );
+//DECLARE( test_expminus );
 DECLARE( test_linear );
 
 void
 register_tests()
 {
-   REGISTER( test_expminus );
+//   REGISTER( test_expminus );
    REGISTER( test_linear );
 }
 
