1aff3e54f2b37406eefe894217d2782c5a540e03
Ricardo Fabbri <rfabbri@gmail.com>
2019-06-22 14:02:35 -0300

    move to algo mostly done

--- tests/test_driver.cxx
+++ tests/test_driver.cxx
@@ -1,15 +1,11 @@
 #include <testlib/testlib_register.h>
 
 DECLARE( test_minus );
-DECLARE( test_expminus );
-DECLARE( test_linear );
 
 void
 register_tests()
 {
    REGISTER( test_minus );
-   REGISTER( test_expminus );
-   REGISTER( test_linear );
 }
 
 DEFINE_MAIN;
