de35a2e5f0250ba6544a65941db12dc1ce7e719c
Ricardo Fabbri <rfabbri@gmail.com>
2019-11-22 11:12:59 -0300

    simpler travis for reference

--- scripts/synthdata_batch.sh
+++ scripts/synthdata_batch.sh
@@ -1,6 +1,5 @@
 stamp=100-triplets
-workdir=/Users/rfabbri/cprg/vxlprg/lemsvpe/minus/scripts/results-synth/work-$stamp
-mkdir $workdir 
+eval_dir=/Users/rfabbri/cprg/vxlprg/lemsvpe/minus/scripts/results-synth/work-$stamp
 
 # echoerr() { echo "LOG $@" 1>&2; }
 echoerr() { printf "LOG %s\n" "$*" >&2; }
@@ -8,16 +7,31 @@ echoerr() { printf "LOG %s\n" "$*" >&2; }
 echoerr started sequential minus tester
 set -x
 
+if [ ! -d $eval_dir ]; then
+  echoerr creating directory $eval_dir
+  if ! mkdir $eval_dir; then
+    echoerr synthdata_batch: could not create directory $eval_dir.
+    exit 1
+  fi
+  # if it exists, we keep dumping files at it, differentiated by PID
+#else
+#  echo cleaning up $eval_dir
+#  rm -f $eval_dir/*
+fi
+
+cp $0 $eval_dir/script.$$
+
+
 while IFS= read -r sample_id || [ -n "$sample_id" ]
 do
   sample_id_tr=`echo $sample_id |tr \  -`
-  #mstdout=$workdir/${sample_id_tr}-$stamp-minus-synth-batch-stdout.$$;
+  #mstdout=$eval_dir/${sample_id_tr}-$stamp-minus-synth-batch-stdout.$$;
   mstdout=/dev/null
-  mstderr=$workdir/${sample_id_tr}-$stamp-minus-synth-batch-stderr.$$;
+  mstderr=$eval_dir/${sample_id_tr}-$stamp-minus-synth-batch-stderr.$$;
   minus-chicago-synth $sample_id 1>$mstdout 2>$mstderr
   solver_fail="$?"
-  failfile=$workdir/${sample_id_tr}-$stamp-fail.$$;
+  failfile=$eval_dir/${sample_id_tr}-$stamp-fail.$$;
   echo $solver_fail > $failfile
-  tfile=$workdir/${sample_id_tr}-$stamp-time.$$;
+  tfile=$eval_dir/${sample_id_tr}-$stamp-time.$$;
   grep 'Time of solver' $mstderr |grep -o '[0-9][0-9]*ms'|grep -o '[0-9][0-9]*' > $tfile
 done < 100-configurations-synthdata #tiny-configurations-synthdata 
