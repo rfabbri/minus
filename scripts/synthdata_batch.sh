784504db1984fe5522568bb3c91c39f6030481c5
Ricardo Fabbri <rfabbri@gmail.com>
2019-11-22 05:02:30 -0300

    experimental large scale tests

--- scripts/synthdata_batch.sh
+++ scripts/synthdata_batch.sh
@@ -1,5 +1,23 @@
-while IFS="" read -r p || [ -n "$p" ]
+stamp=100-triplets
+workdir=/Users/rfabbri/cprg/vxlprg/lemsvpe/minus/scripts/results-synth/work-$stamp
+mkdir $workdir 
+
+# echoerr() { echo "LOG $@" 1>&2; }
+echoerr() { printf "LOG %s\n" "$*" >&2; }
+
+echoerr started sequential minus tester
+set -x
+
+while IFS= read -r sample_id || [ -n "$sample_id" ]
 do
-  #printf '%s\n' "$p"
-  synthdata $p 
-done < 100-configurations-synthdata
+  sample_id_tr=`echo $sample_id |tr \  -`
+  #mstdout=$workdir/${sample_id_tr}-$stamp-minus-synth-batch-stdout.$$;
+  mstdout=/dev/null
+  mstderr=$workdir/${sample_id_tr}-$stamp-minus-synth-batch-stderr.$$;
+  minus-chicago-synth $sample_id 1>$mstdout 2>$mstderr
+  solver_fail="$?"
+  failfile=$workdir/${sample_id_tr}-$stamp-fail.$$;
+  echo $solver_fail > $failfile
+  tfile=$workdir/${sample_id_tr}-$stamp-time.$$;
+  grep 'Time of solver' $mstderr |grep -o '[0-9][0-9]*ms'|grep -o '[0-9][0-9]*' > $tfile
+done < 100-configurations-synthdata #tiny-configurations-synthdata 
