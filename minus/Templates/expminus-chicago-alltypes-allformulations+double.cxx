c7dbddca0f75f58788f3c7e45955db5382491373
Ricardo Fabbri <rfabbri@gmail.com>
2019-11-22 23:08:33 -0300

    starting expminus side by side to minus at first

---
+++ minus/Templates/expminus-chicago-alltypes-allformulations+double.cxx
@@ -0,0 +1,7 @@
+// Helps instantiate and reuse code,
+// when including the .hxx directly causes inefficiencies and slowdown
+// 
+// \author Ricardo Fabbri based on original code by Anton Leykin 
+// \date Created: Fri Feb  8 17:42:49 EST 2019
+#include <minus/expminus.hxx>
+template class expminus_core<chicago14a, double>;
