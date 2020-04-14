14f668a5817b0f94ae73f03fa9490001f2c8be24
Ricardo Fabbri <rfabbri@gmail.com>
2020-04-14 08:06:45 -0300

    minus cleveland changes

--- cmd/minus-cleveland-raw.cxx
+++ cmd/minus-cleveland-raw.cxx
@@ -10,7 +10,7 @@
 #include <minus/minus.h>
 
 #define Float double
-typedef minus<chicago14a> M;
+typedef minus<cleveland14a> M;
 static constexpr Float tol = 1e-3;
 typedef std::complex<Float> complex;
 using namespace std::chrono;
@@ -19,7 +19,7 @@ using namespace std::chrono;
 // If you want to play with different start sols,
 // write another program that accepts start sols in runtime,
 // but keep this one lean & mean.
-#include <minus/chicago14a-default.hxx> 
+#include <minus/cleveland14a-default.hxx> 
 // We include it separately so they don't clutter this app,
 // neither minus.h, and can be reused by other progs
 // TODO(developer note): make this part of Minus' template as a specialization. 
