d699e483f2d30b4b11907e229ce5c181007e8127
Ricardo Fabbri <rfabbri@gmail.com>
2020-04-14 07:47:07 -0300

    starting cleveland port from curent PLMP generic solver - files

---
+++ minus/Templates/minus-cleveland-alltypes-allformulations+double.cxx
@@ -0,0 +1,9 @@
+// Helps instantiate and reuse code,
+// when including the .hxx directly causes inefficiencies and slowdown
+// 
+// \author Ricardo Fabbri 
+// \date Created: Fri Feb  8 17:42:49 EST 2019
+#include <minus/minus.hxx>
+template class minus_core<cleveland14a, double>;
+template class minus_io_shaping<cleveland14a, double>;
+template class minus_util<double>;
