b03dc1f554c13572adc4c878275f9c9138f54bce
Ricardo Fabbri <rfabbri@gmail.com>
2023-10-08 13:51:53 -0300

    [Trifocal] visual studio bug workaround

--- minus/internal-util.h
+++ minus/internal-util.h
@@ -6,9 +6,13 @@
 
 
 // Visual studio bug work-around
-#ifndef M_PI_2
-#define M_PI_2 1.57079632679489661923
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
 #endif
+#ifndef M_PI_2
+    #define M_PI_2 1.57079632679489661923
+#endif
+
 
 namespace MiNuS {
   
