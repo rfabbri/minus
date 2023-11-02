2e49582024cb772a8d7c68511e919ea1f3d682a1
Ricardo Fabbri <rfabbri@gmail.com>
2023-11-02 17:35:38 -0300

    Turning off Cleveland focusing on Chicago for the time being

--- minus/Templates/minus-cleveland-alltypes-allformulations+double.cxx
+++ minus/Templates/minus-cleveland-alltypes-allformulations+double.cxx
@@ -3,15 +3,15 @@
 // 
 // \author Ricardo Fabbri 
 // \date Created: Fri Feb  8 17:42:49 EST 2019
-#include <minus/minus.hxx>
-#include <minus/cleveland14a-default-data.hxx>
+//#include <minus/minus.hxx>
+//#include <minus/cleveland14a-default-data.hxx>
 
 namespace MiNuS {
   
-template class minus_core<cleveland14a, double>;
-template struct minus_io_14a<cleveland14a, double>;
-template struct minus_io<cleveland14a, double>;
-template struct minus<cleveland14a, double>;
-template struct minus_data<cleveland14a, double>;
+//template class minus_core<cleveland14a, double>;
+//template struct minus_io_14a<cleveland14a, double>;
+//template struct minus_io<cleveland14a, double>;
+//template struct minus<cleveland14a, double>;
+//template struct minus_data<cleveland14a, double>;
 
 }
