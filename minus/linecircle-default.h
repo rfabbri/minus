1466892d97996fd297262b05e76ee215897d508b
Ricardo Fabbri <rfabbri@gmail.com>
2024-06-15 21:28:37 -0700

    default c++ file for 2x2

--- minus/linecircle-default.h
+++ minus/linecircle-default.h
@@ -1,15 +1,15 @@
-#ifndef chicago_default_h_
-#define chicago_default_h_
+#ifndef linecircle_default_h_
+#define linecircle_default_h_
 // Include this last of all in user app
 
 #include "minus.h"
-#include "chicago14a-default-data.h" // default start system etc
+#include "linecircle-default-data.h" // default start system etc
 
-// Convenience types to use Minus and the Chicago problem
-typedef MiNuS::minus_core<MiNuS::chicago> M;
-typedef MiNuS::minus_io<MiNuS::chicago> io;
-typedef MiNuS::minus_io_14a<MiNuS::chicago> io14;
-typedef MiNuS::minus_data<MiNuS::chicago, double> data;
-extern template struct MiNuS::minus_data<MiNuS::chicago, double>;
+// Convenience types to use Minus and the linecircle problem
+typedef MiNuS::minus_core<MiNuS::linecircle> M;
+typedef MiNuS::minus_io<MiNuS::linecircle> io;
+typedef MiNuS::minus_io_14a<MiNuS::linecircle> io14;
+typedef MiNuS::minus_data<MiNuS::linecircle, double> data;
+extern template struct MiNuS::minus_data<MiNuS::linecircle, double>;
 
-#endif   // chicago_default_h_
+#endif   // linecircle_default_h_
