8a6bac538ba97d5b5369d3a3886da020e4627b28
Ricardo Fabbri <rfabbri@gmail.com>
2026-02-26 17:03:51 -0300

    generic Eigen partialpiv lsolve works as convenience for new solvers

--- minus/linecircle2a-lsolve.hxx
+++ minus/linecircle2a-lsolve.hxx
@@ -22,15 +22,14 @@ lsolve(
     Map<Matrix<C<F>, minus_core<P,F>::f::nve, minus_core<P,F>::f::nve +1>,Aligned> & __restrict m, 
     C<F> __restrict *ux)
 {
-  C<F> * const x= reinterpret_cast<C<F> *> (__builtin_assume_aligned(ux,64));
+  C<F> * const x = reinterpret_cast<C<F> *> (__builtin_assume_aligned(ux,64));
   //asm("#------ Lsolve begin"); // there is too many vmovsd moving data. It is sub-vectorized, using only xmm no y or zmm
   typedef minus_core<P, F> M;
-  static constexpr unsigned char rows = M::f::nve;
 
   // Noob ----------------------------------------------------------------------
   PartialPivLU<Matrix<C<F>, M::f::nve, M::f::nve> > lu;
   Map<Matrix<C<F>, M::f::nve, 1>, Aligned> x_eigen(x);
-  x_eigen = lu.compute(m.template block<rows,rows>(0,0)).solve(m.col(M::f::nve));
+  x_eigen = lu.compute(m.template block<M::f::nve,M::f::nve>(0,0)).solve(m.col(M::f::nve));
 
   // Pro -----------------------------------------------------------------------
   // Fine-tune Eigen's LU to your problem size and structure, e.g., limit pivoting
